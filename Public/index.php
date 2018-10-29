022743ce4eac8200ddfb12e290f6f6b50f363f75
HabibAroua <habibha.aroua82@gmail.com>
2018-10-29 23:07:16 +0100

    edit code

--- Public/index.php
+++ Public/index.php
@@ -1,7 +1,16 @@
 <!doctype html>
 <html>
     <head>
-        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="https://getbootstrap.com/favicon.ico">
+        <title>Signin Template for Bootstrap</title>
+        <!-- Bootstrap core CSS -->
+        <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="https://getbootstrap.com/docs/4.0/examples/sign-in/signin.css" rel="stylesheet">
     </head>
     <body>
         <?php
