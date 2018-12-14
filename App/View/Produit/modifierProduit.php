8dcf3c56ae8241e7d3c3d90847d667a78779f4ab
HabibAroua <habibha.aroua82@gmail.com>
2018-12-14 23:28:01 +0100

    correct the stat

--- App/View/Produit/modifierProduit.php
+++ App/View/Produit/modifierProduit.php
@@ -93,16 +93,16 @@
                             else
                             {
                                 echo
-                                    "<script>
-                                        swal(
-                                                {
-                                                    title: 'Erreur',
-                                                    text: 'Les fichiers png ou jpg sont acceptable',
-                                                    icon: 'warning',
-                                                    button: 'OK',
-                                                }
-                                            );
-                                    </script>";
+                                "<script>
+                                    swal(
+                                            {
+                                                 title: 'Erreur',
+                                                text: 'Les fichiers png ou jpg sont acceptable',
+                                                 icon: 'warning',
+                                                 button: 'OK',
+                                            }
+                                        );
+                                </script>";
                             }
                         }
                     ?>
