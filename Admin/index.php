42e08da9bb939dc02414ab79fea228127fa2de9c
HabibAroua <habibha.aroua82@gmail.com>
2018-12-16 19:51:16 +0100

    edit proje

--- Admin/index.php
+++ Admin/index.php
@@ -6,110 +6,110 @@
          <link href="bootstrap/bootstrap.min.css" rel="stylesheet"/>    
         <link href="bootstrap/bootstrap-theme.css" rel="stylesheet"/>
         <link href="fonts/OleoScript-Regular.ttf" rel="stylesheet" />
-    </head>
-    <style>
-        @font-face
-        {
-            font-family:Fuentechida;
-            src:url(fonts/OleoScript-Regular.ttf);
-        }
+		<style>
+            @font-face
+            {
+                font-family:Fuentechida;
+                src:url(fonts/OleoScript-Regular.ttf);
+            }
     
-        body
-        {
-            background-image: url(image/bb.jpg);
-            background-size:cover;
-            background-repeat: no-repeat;
-            background-attachment:fixed;
-        }
+            body
+            {
+                background-image: url(image/bb.jpg);
+                background-size:cover;
+                background-repeat: no-repeat;
+                background-attachment:fixed;
+            }
          
-        .formulaire
-        {
-            transition: 2s;
-            margin-top: 100px;
-            width: 30%;
-            box-shadow: 0px 0px 40px violet,0px 0px 80px violet;
-        }
+            .formulaire
+            { 
+                transition: 2s;
+                margin-top: 100px;
+                width: 30%;
+                box-shadow: 0px 0px 40px violet,0px 0px 80px violet;
+            }
         
-        .formulaire:hover
-        {
-            transition: .8s;
-            background-color: rgba(0,0,0,.5);
-        }
+            .formulaire:hover
+            {
+                transition: .8s;
+                background-color: rgba(0,0,0,.5);
+            }
         
-        .login
-        {
-            height: 75px;
-            margin-top: 40px;
-        }
+            .login
+            {
+                height: 75px;
+                margin-top: 40px;
+            }
         
-        .espace
-        {
-            margin-top: 40px;
-        }
+            .espace
+            {
+                margin-top: 40px;
+            }
         
-        fieldset
-        {
-            transition: 2s;
-            margin-bottom: 50px;
-            border-color: wheat;
-            border-style: groove;
-            border-width: 5px;
-            border-radius: 20px;
-        }
+            fieldset
+            {
+                transition: 2s;
+                margin-bottom: 50px;
+                border-color: wheat;
+                border-style: groove;
+                border-width: 5px;
+                border-radius: 20px;
+            }
         
-        h3,h4
-        {
-            color:white;
-            text-align: center;
-            font-family: fuentechida;
-        }
+            h3,h4
+            {
+                color:white;
+                text-align: center;
+                font-family: fuentechida;
+            }
   
-    .Input
-	{
-        transition: .8s;
-        background-color: rgba(0,0,0,.5);
-        color: darkcyan;
-        border-color:#006;
-	    border-bottom-color:blueviolet;
-        border-bottom-style:groove;
-	    border-left:none;
-	    border-right:none;
-	    border-top:none;
-        border-width: 4px;
-    }
-
-    .Input:hover{
-        transition: .8s;
-	background-color:slategray;
-	box-shadow:inset;
-        border-bottom-color:saddlebrown;
-	}
-    
-    .Input:focus{
-        transition: .8s;
-	border-bottom-color:salmon;
-    }
-
-
-
-    @media screen and (max-width:750px) {
-        
-        .login{
-            height: 50px;
-
-        }
-        .formulaire{
-            transition: 2s;
-            width: 95%;
-            margin-top: 10px;
-        }
-    }
-</style>
-
-
-<body>
-    <?php
-        require_once('../App/View/default/loginAdmin.php');
-    ?>
-</body>
+            .Input
+	        {
+			    transition: .8s;
+                background-color: rgba(0,0,0,.5);
+                color: darkcyan;
+                border-color:#006;
+	            border-bottom-color:blueviolet;
+                border-bottom-style:groove;
+	            border-left:none;
+	            border-right:none;
+	            border-top:none;
+                border-width: 4px;
+		    }
+	 	
+		    .Input:hover
+		    {
+			    transition: .8s;
+	            background-color:slategray;
+	            box-shadow:inset;
+                border-bottom-color:saddlebrown;
+		    }
+		
+		    .Input:focus
+		    {
+			    transition: .8s;
+			    border-bottom-color:salmon;
+		    }
+		
+		    @media screen and (max-width:750px)
+		    {
+			    .login
+			    {
+				    height: 50px;
+			    }
+			
+			    .formulaire
+			    {
+				    transition: 2s;
+                    width: 95%;
+                    margin-top: 10px;
+			    }
+		    }
+        </style>
+    </head>
+    <body>
+        <?php
+            require_once('../App/View/default/loginAdmin.php');
+        ?>
+    </body>
 </html>
