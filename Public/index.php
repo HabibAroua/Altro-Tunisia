f81cd16001ede7b78430d2fe40e9da960e43218c
HabibAroua <habibha.aroua82@gmail.com>
2019-02-01 18:30:00 +0100

    add jquery file

--- Public/index.php
+++ Public/index.php
@@ -11,7 +11,7 @@
         <link href="css/style.css" rel="stylesheet" >
         <script src="javascript/sweetalert.min.js"></script>
 		<link href="css/sweetalert2.css" rel="stylesheet">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="javascript/bootstrap.min.js"></script>
         <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="javascript/Alert.js"></script>
