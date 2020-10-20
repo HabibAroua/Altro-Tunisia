21409704e687f29baf0933a80ef3c009a3d3f461
Habib Aroua <habib.aroua@sesame.com.tn>
2020-10-20 22:58:15 +0100

    Update Admin.php

--- App/Model/Admin.php
+++ App/Model/Admin.php
@@ -1,68 +1,68 @@
 <?php
-require_once('../App/DataBase/query.php');
+	require_once('../App/DataBase/query.php');
 
-    class Admin
-    {
-        private $login;
-        private $motDePAsse;
-        private $nom;
-        private $prenom;
-        
-        public function __construct()
-        {
+    	class Admin
+    	{
+        	private $login;
+        	private $motDePAsse;
+        	private $nom;
+        	private $prenom;
 
-        }
+		public function __construct()
+		{
 
-        public function getLogin()
-        {
-            return $this->login;
-        }
+		}
 
-        public function getMotDePasse()
-        {
-            return $this->motDePAsse;
-        }
-        
-        public function getNom()
-        {
-            return $this->nom;
-        }
+		public function getLogin()
+		{
+			return $this->login;
+		}
 
-        public function getPrenom()
-        {
-            return $this->prenom;
-        }
-        
-        public function getDate_res()
-        {
-            return $this->date_res;
-        }
-        
-        public function add()
-        {
-            return input("insert into client values('$this->login','$this->motDePAsse','$this->nom','$this->prenom','this->date_res')");
-        }
+		public function getMotDePasse()
+		{
+			return $this->motDePAsse;
+		}
 
-        public function update($login)
-        {
-            return input("update table admin set login='$this->login' , motDePasse='$this->motDePasse' , nom='$this->nom' , prenom='$this->prenom' where login='$login' ");
-        }
-        
-        public function delete()
-        {
-            return input("delete admin where login='$login'");
-        }
+		public function getNom()
+		{
+			return $this->nom;
+		}
 
-        public function allAdmin()
-        {
-            $res=output("select * from admin ");
-            $i=0;
-            while($tab=$res->fetch(PDO::FETCH_NUM))
-            {
-              $T[$i]=$admin = array('login'=>$tab[0],'password'=>$tab[1],);
-              $i++;
-		 	}
-            return $T;
-        }
-    }
+		public function getPrenom()
+		{
+			return $this->prenom;
+		}
 
+		public function getDate_res()
+		{
+			return $this->date_res;
+		}
+
+		public function add()
+		{
+			return input("insert into client values('$this->login','$this->motDePAsse','$this->nom','$this->prenom','this->date_res')");
+		}
+
+		public function update($login)
+		{
+			return input("update table admin set login='$this->login' , motDePasse='$this->motDePasse' , nom='$this->nom' , prenom='$this->prenom' where login='$login' ");
+		}
+
+		public function delete()
+		{
+			return input("delete admin where login='$login'");
+		}
+
+		public function allAdmin()
+		{
+			$res=output("select * from admin ");
+		    	$i=0;
+		    	while($tab=$res->fetch(PDO::FETCH_NUM))
+		    	{
+				$T[$i]=$admin = array('login'=>$tab[0],'password'=>$tab[1],);
+		      		$i++;
+			}
+		    	return $T;
+		}
+	}
+?>
